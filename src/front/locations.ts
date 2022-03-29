6345a7557b6acdc63976b359aca29c65e45dc2bf
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 14:28:04 +0100

    add cardiff location

---
+++ src/front/locations.ts
@@ -0,0 +1,1 @@
+export const cardiff = { center: { lng: -3.18, lat: 51.48 }, zoom: 12 };
--- src/front/main.ts
+++ src/front/main.ts
@@ -1,3 +1,4 @@
+import { cardiff } from "./locations";
 import { getMapsAPI } from "./mapsAPI";
 
 main();
@@ -5,17 +6,7 @@ main();
 async function main() {
   const mapsAPI = await getMapsAPI();
   const map = new mapsAPI.Map(document.getElementById("map-root"), {
-    center: { lat: 51.4539, lng: 3.1694 },
-    zoom: 3,
-  });
-
-  ["lng", "lat", "zoom"].map((s) => {
-    document.getElementById(s).onchange = () => {
-      map.setCenter({
-        lng: parseFloat(document.getElementById("lng").value),
-        lat: parseFloat(document.getElementById("lat").value),
-      });
-      map.setZoom(parseFloat(document.getElementById("zoom").value));
-    };
+    center: cardiff.center,
+    zoom: cardiff.zoom,
   });
 }
