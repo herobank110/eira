1e1526752c1cb1977b4b39713d52b5bf7e93b141
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 13:58:34 +0100

    load map

--- src/front/main.ts
+++ src/front/main.ts
@@ -1,6 +1,12 @@
 import { getMapsAPI } from "./mapsAPI";
 
-getMapsAPI().then((maps) => {
-  console.log("maps:", maps);
-});
-    
+main();
+
+async function main() {
+  getMapsAPI().then((maps) => {
+    new maps.Map(document.getElementById("map-root"), {
+      center: { lat: 51.4539, lng: 3.1694 },
+      zoom: 3
+    });
+  });
+}
