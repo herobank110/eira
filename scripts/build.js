7ed4a7011fdd70f514a9497e805d72e702a9b003
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 12:21:45 +0100

    fix types

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
-import { REPO_ROOT } from "./constants";
-import { exec } from "./utils";
+import { REPO_ROOT } from "./constants.js";
+import { exec } from "./utils.js";
 
 exec("parcel build --cache-dir=build/cache --dist-dir=build/front --public-url=. src/front/index.html", { cwd: REPO_ROOT });
