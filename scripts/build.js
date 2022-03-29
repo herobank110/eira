5f08f819b3661f6ae2668d3c8a79407d956635c9
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 12:52:01 +0100

    apache config

--- scripts/build.js
+++ scripts/build.js
@@ -4,13 +4,12 @@ import { REPO_ROOT } from "./constants.js";
 import { exec } from "./utils.js";
 
 const build = path.join(REPO_ROOT, "build");
-const src = path.join(REPO_ROOT, "src");
-const prod = path.join(build, "prod");
 const cache = path.join(build, "cache");
-const front = path.join(prod, "front");
+const prod = path.join(build, "prod");
 const api = path.join(prod, "api");
+const src = path.join(REPO_ROOT, "src");
 
-exec(`parcel build --cache-dir="${cache}" --dist-dir="${front}" --public-url=. ${src}/front/index.html`, { cwd: REPO_ROOT });
+exec(`parcel build --cache-dir="${cache}" --dist-dir="${prod}" --public-url=. ${src}/front/index.html`, { cwd: REPO_ROOT });
 
 if (!fsExtra.existsSync(api)) {
     console.log('Creating API link');
