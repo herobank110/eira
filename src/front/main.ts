da0ef0b4a1e5e33701cf7c663e743c4e3db0d2d8
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 14:20:34 +0100

    set debug

--- src/front/main.ts
+++ src/front/main.ts
@@ -3,10 +3,19 @@ import { getMapsAPI } from "./mapsAPI";
 main();
 
 async function main() {
-  getMapsAPI().then((maps) => {
-    new maps.Map(document.getElementById("map-root"), {
-      center: { lat: 51.4539, lng: 3.1694 },
-      zoom: 3
-    });
+  const mapsAPI = await getMapsAPI();
+  const map = new mapsAPI.Map(document.getElementById("map-root"), {
+    center: { lat: 51.4539, lng: 3.1694 },
+    zoom: 3,
+  });
+
+  ["lng", "lat", "zoom"].map((s) => {
+    document.getElementById(s).onchange = () => {
+      map.setCenter({
+        lng: document.getElementById("lng").value,
+        lat: document.getElementById("lat").value,
+      });
+      map.setZoom(document.getElementById("zoom").value);
+    };
   });
 }
