0b63de1bde8fd919a2c9d31d8ed2e738efe87880
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 12:47:51 +0100

    refactor fssync

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,18 @@
+import fsExtra from "fs-extra";
+import path from "path";
 import { REPO_ROOT } from "./constants.js";
 import { exec } from "./utils.js";
 
-exec("parcel build --cache-dir=build/cache --dist-dir=build/front --public-url=. src/front/index.html", { cwd: REPO_ROOT });
+const build = path.join(REPO_ROOT, "build");
+const src = path.join(REPO_ROOT, "src");
+const prod = path.join(build, "prod");
+const cache = path.join(build, "cache");
+const front = path.join(prod, "front");
+const api = path.join(prod, "api");
+
+exec(`parcel build --cache-dir="${cache}" --dist-dir="${front}" --public-url=. ${src}/front/index.html`, { cwd: REPO_ROOT });
+
+if (!fsExtra.existsSync(api)) {
+    console.log('Creating API link');
+    fsExtra.createSymlinkSync(path.join(src, "api"), api, "junction");
+}
