9e3e218d4cc07af5711d1f3e7afaa30f818ea854
David Kanekanian <dkanekanian1@gmail.com>
2022-03-29 14:35:32 +0100

    events test

--- src/front/main.ts
+++ src/front/main.ts
@@ -5,8 +5,17 @@ main();
 
 async function main() {
   const mapsAPI = await getMapsAPI();
+
   const map = new mapsAPI.Map(document.getElementById("map-root"), {
     center: cardiff.center,
     zoom: cardiff.zoom,
   });
+
+  const marker = new mapsAPI.Marker({
+    map,
+    position: { lng: -3.1632, lat: 51.4648 },
+  });
+  marker.addListener("click", () => console.log("click"));
+  marker.addListener("mouseover", () => console.log("mouseover"));
+  marker.addListener("mouseout", () => console.log("mouseout"));
 }
