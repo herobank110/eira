afa08284021511cadf9cf7ff65900cd5770cc80d
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 11:12:37 +0100

    add all maps markers

--- src/front/locations.ts
+++ src/front/locations.ts
@@ -1,1 +1,28 @@
 export const cardiff = { center: { lng: -3.18, lat: 51.48 }, zoom: 12 };
+
+export const markers = [
+  {
+    name: "Cardiff Bay",
+    position: { lat: 51.4539, lng: -3.1694 },
+  },
+  {
+    name: "Roath Park",
+    position: { lat: 51.5077, lng: -3.1739 },
+  },
+  {
+    name: "Cardiff Castle",
+    position: { lat: 51.4822, lng: -3.1812 },
+  },
+  {
+    name: "St David's Shopping Centre",
+    position: { lat: 51.4807, lng: -3.1752 },
+  },
+  {
+    name: "Principality Stadium",
+    position: { lat: 51.4782, lng: -3.1826 },
+  },
+  {
+    name: "Wales Millennium Stadium",
+    position: { lat: 51.4648, lng: -3.1632 },
+  },
+];
--- src/front/main.ts
+++ src/front/main.ts
@@ -1,25 +1,25 @@
-import { cardiff } from "./locations";
+import { cardiff, markers } from "./locations";
 import { getMapsAPI } from "./mapsAPI";
 
 import tippy, { followCursor } from "tippy.js";
 import "tippy.js/dist/tippy.css";
-setTimeout(() => {
-  let a = tippy(document.body, {
-    followCursor: true,
-    content: "hello there",
-    plugins: [followCursor],
-    showOnCreate: true,
-    placement: "bottom",
-    offset: [0, 20],
-    arrow: false,
-  });
-  const el = document.getElementById("tippy-" + a.id);
-  setTimeout(() => {
-    el.style.transform = "translate3d(100px, 100px, 0px)";
-  }, 0);
-}, 2000);
+// setTimeout(() => {
+//   let a = tippy(document.body, {
+//     followCursor: true,
+//     content: "hello there",
+//     plugins: [followCursor],
+//     showOnCreate: true,
+//     placement: "bottom",
+//     offset: [0, 20],
+//     arrow: false,
+//   });
+//   const el = document.getElementById("tippy-" + a.id);
+//   setTimeout(() => {
+//     el.style.transform = "translate3d(100px, 100px, 0px)";
+//   }, 0);
+// }, 2000);
 
-// main();
+main();
 
 async function main() {
   const mapsAPI = await getMapsAPI();
@@ -30,18 +30,10 @@ async function main() {
     mapId: "3f60a325ef504f09",
   });
 
-  const marker = new mapsAPI.Marker({
-    map,
-    position: { lng: -3.1632, lat: 51.4648 },
-  });
-  marker.addListener("click", () => console.log("click"));
-  marker.addListener("mouseover", () => console.log("mouseover"));
-  marker.addListener("mouseout", () => console.log("mouseout"));
-  const m2 = new mapsAPI.Marker({
-    map,
-    position: { lng: -3.18, lat: 51.4648 },
-  });
-  m2.addListener("click", () => console.log("click"));
-  m2.addListener("mouseover", () => console.log("mouseover"));
-  m2.addListener("mouseout", () => console.log("mouseout"));
+  for (const markerData of markers) {
+    const mapMarker = new mapsAPI.Marker({ map, position: markerData.position });
+    mapMarker.addListener("click", () => console.log("click"));
+    mapMarker.addListener("mouseover", () => console.log("mouseover"));
+    mapMarker.addListener("mouseout", () => console.log("mouseout"));
+  }
 }
