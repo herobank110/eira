17cfd54f083d36209d4758f5ca25c08ac4178abc
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 17:37:58 +0100

    add db file

---
+++ scripts/db.sql
@@ -0,0 +1,60 @@
+# Run this as root
+
+create user eira@localhost identified by 'aaaaaa123';
+create database eira;
+use eira;
+grant all on eira.* to eira@localhost;
+
+create table landmark (
+    id int not null primary key auto_increment,
+    name varchar(120) not null,
+    lat float not null,
+    lng float not null,
+    thumb varchar(255) not null,
+    desc_ varchar(3000) not null
+) engine InnoDB;
+
+insert into landmark (name, lat, lng, thumb, desc_)
+values 
+    (
+        "Cardiff Bay",
+        51.4539,
+        -3.1694,
+        "res/bay.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    (
+        "Roath Park",
+        51.5077,
+        -3.1739,
+        "res/roath.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    (
+        "Cardiff Castle",
+        51.4822,
+        -3.1812,
+        "res/castle.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    (
+        "St David's Shopping Centre",
+        51.4807,
+        -3.1752,
+        "res/shop.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    (
+        "Principality Stadium",
+        51.4782,
+        -3.1826,
+        "res/principality.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    ),
+    (
+        "Wales Millennium Stadium",
+        51.4648,
+        -3.1632,
+        "res/millennium.jpg",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    );
