f4094daeca7029640142c4949fe3117105d34d7e
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 17:45:01 +0100

    add res link

--- scripts/build.js
+++ scripts/build.js
@@ -7,11 +7,17 @@ const build = path.join(REPO_ROOT, "build");
 const cache = path.join(build, "cache");
 const prod = path.join(build, "prod");
 const api = path.join(prod, "api");
+const res = path.join(prod, "res");
 const src = path.join(REPO_ROOT, "src");
 
 exec(`parcel build --cache-dir="${cache}" --dist-dir="${prod}" --public-url=. ${src}/front/index.html`, { cwd: REPO_ROOT });
 
 if (!fsExtra.existsSync(api)) {
-    console.log('Creating API link');
+    console.log('Creating api link');
     fsExtra.createSymlinkSync(path.join(src, "api"), api, "junction");
+}
+
+if (!fsExtra.existsSync(res)) {
+    console.log('Creating res link');
+    fsExtra.createSymlinkSync(path.join(REPO_ROOT, "res"), res, "junction");
 }
