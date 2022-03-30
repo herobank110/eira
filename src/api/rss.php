c6fc7a037f0b0cd09b0d5959354b8bff40170bac
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 19:17:24 +0100

    add rss feed

---
+++ src/api/rss.php
@@ -0,0 +1,35 @@
+<?php
+require_once "./db_connection.php";
+
+$baseUrl = "$_SERVER[REQUEST_SCHEME]://$_SERVER[HTTP_HOST]";
+
+$db = get_or_create_db_connection();
+$result = $db->query("select name, desc_ from landmark");
+$items = "";
+while ($row = $result->fetch_assoc()) {
+    $items .= "
+ <item>
+  <title>$row[name]</title>
+  <description>$row[desc_]</description>
+  <link>$baseUrl</link>
+  <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+ </item>";
+}
+
+$xml = "
+<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<rss version=\"2.0\">
+<channel>
+ <title>Cardiff Landmarks</title>
+ <description>My RSS Feed!</description>
+ <link>http://eira.mooo.com</link>
+ <copyright>2020 Example.com All rights reserved</copyright>
+ <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000</lastBuildDate>
+ <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+ <ttl>1800</ttl>
+ $items
+</channel>
+</rss>";
+
+echo $xml;
+header("Content-Type: application/rss+xml; charset=ISO-8859-1");
