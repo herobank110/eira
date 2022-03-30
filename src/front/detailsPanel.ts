8486b5c03a8f22bbff5520541d9d0db0beae2c66
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 13:45:59 +0100

    add jquery

--- src/front/detailsPanel.ts
+++ src/front/detailsPanel.ts
@@ -1,18 +1,21 @@
 import { Offcanvas } from "mdb-ui-kit";
+import $ from "jquery";
 
 type DetailsPanelProps = {
   title: string;
-  thumbnail: string;
+  thumb: string;
   desc: string;
 };
 
 export function setDetails(props: DetailsPanelProps) {
-  const el = document.getElementById("details-panel");
+  const el = $("#details-panel");
+  el.find(".offcanvas-title").text(props.title);
+  el.find(".details-panel--thumb").css(
+    "background-image",
+    `url('${props.thumb}')`
+  );
+  el.find(".details-panel--desc").text(props.desc);
 
-  // TODO: insert data
-
-  const offcanvas = new Offcanvas(el);
-  if (!offcanvas._isShown) {
-    offcanvas.show();
-  }
+  const offcanvas = new Offcanvas(el[0]);
+  offcanvas.show();
 }
