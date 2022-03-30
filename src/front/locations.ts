47511c018962a22279e4bc2f451f91604cce5c28
David Kanekanian <dkanekanian1@gmail.com>
2022-03-30 13:07:28 +0100

    cardiff zoom

--- src/front/locations.ts
+++ src/front/locations.ts
@@ -1,4 +1,4 @@
-export const cardiff = { center: { lng: -3.18, lat: 51.48 }, zoom: 12 };
+export const cardiff = { center: { lng: -3.18, lat: 51.48 }, zoom: 13 };
 
 export type MarkerData = {
   name: string;
