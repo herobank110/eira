0a82c9887ecd095787c012bf3606ca8ffb0343e2
David Kanekanian <dkanekanian1@gmail.com>
2022-04-01 13:43:43 +0100

    integrate json api key

--- src/front/mapsAPI.ts
+++ src/front/mapsAPI.ts
@@ -1,18 +1,18 @@
 /** @file helpers for google maps API */
 
 import { Loader } from "@googlemaps/js-api-loader";
+import { googleMapsAPIKey } from "../../eiraConfig.json";
 
 // namespace alias
 var google = { maps: null };
 // @ts-ignore
 export import MapsAPI = google.maps;
 
-const g_apiKey = "AIzaSyBb5wFapnvB4Z1lN9OA1ruUW6eQ65YebmU";
 let g_API: typeof MapsAPI | undefined;
 
 export async function getMapsAPI() {
   if (!g_API) {
-    const loader = new Loader({ apiKey: g_apiKey });
+    const loader = new Loader({ apiKey: googleMapsAPIKey });
     try {
       g_API = (await loader.load()).maps;
     } catch (error) {
